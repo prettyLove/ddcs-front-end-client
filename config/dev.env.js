de76e596abcf84bf2c05b90c6fcd5bdfaec2a875
master <574286814@qq.com>
2019-10-14 17:02:33 +0800

    初始化

---
+++ config/dev.env.js
@@ -0,0 +1,5 @@
+module.exports = {
+  NODE_ENV: '"development"', // 这里必须带双引号，不然启动命令无法适配到此配置
+  ENV_CONFIG: '"dev"',
+  BASE_API: '"/api"'
+}
---
+++ config/sit.env.js
@@ -0,0 +1,5 @@
+module.exports = {
+  NODE_ENV: '"production"', // 这里必须带双引号，不然启动命令无法适配到此配置
+  ENV_CONFIG: '"sit"',
+  BASE_API: '"/api"'
+}
